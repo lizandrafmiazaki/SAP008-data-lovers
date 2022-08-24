a53efb3e057454f6daeaa40ddac7dd28c252f027
Amanda <amandaoliveirapolari@gmail.com>
2022-08-24 16:56:07 -0300

    add:funções que acompanham os selects + alterações no html e css

--- src/main.js
+++ src/main.js
@@ -1,59 +1,54 @@
-//Aqui vai estar os imports dos filtros do data.js
 import data from './data/lol/lol.js';
 
-//Referenciando o HTML no JS:
-const botaoNome = document.getElementById("select-name");
-const botaoTipo = document.getElementById("select-type");
-const botaoDificuldade = document.getElementById("select-difficulty");
-const botaoLimpar = document.getElementById("btn-clear");
-const botaoRetornar = document.getElementById("btn-return");
+//---------------------------------------------------[início] Testando chamada do data.js
+
+//=> Criei uma constante para salvar todas as chaves do documento data.js:
+const todasAsChaves = Object.keys(data.data);
+
+//=> Peguei a div de saída
+const minhaDiv = document.getElementById("section-change");
+
+//=>Criei uma const pra salvar os names:
+const listaComNames = todasAsChaves.map(chaves => data.data[chaves].name);
+
+//=> Escrevendo a lista no html:
+minhaDiv.innerHTML = listaComNames;
 
 
+//----------------------------------------------------[Fim] Testando chamada do data.js
 
+//----------------------------------------------------------------------------------------[INÍCIO] Monitorando select
+//=> Declaração de variáveis úteis:
+const btnPesquisar = document.getElementById("btn-search");
 
+//=> Função para acompanhar mudanças no select ORDENAR POR
+function capturaSelect(event){
+    let selectParaAcompanhar = document.getElementById("order-by");
+    let capturaTagDoSelectSelecionado = selectParaAcompanhar.options[selectParaAcompanhar.selectedIndex];
+    //=> Sequencia de consoles para testar cada captura:
+    // console.log(selectParaAcompanhar);
+    // console.log(selectParaAcompanhar.options);
+    // console.log(capturaTagDoSelectSelecionado);
 
+    let valueOption = capturaTagDoSelectSelecionado.value;
+    //=>(PODE PULAR) Opção de captura de texto usar linhas 17 e 18:
+    // let textOption = capturaSelectSelecionado.text;
+    // console.log(valueOption,textOption);
 
+    //=> Capturando a tag html que é onde eu quero que saia as informações:
+    let tagHtmlDeSaida = document.getElementById("paragrafoTeste");
 
+    //=> Salvando em uma variável o que eu quero escrever no HTML:
+    let minhaSaida = tagHtmlDeSaida.innerHTML = valueOption;
 
+    //=> Comando padrão para não resetar a página após o evento:
+    event.preventDefault();
 
+    //=> Retorno da minha função:
+    return minhaSaida;
+}
 
+//=> Acompanhamento do Evento Click
+btnPesquisar.addEventListener("click", capturaSelect);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ------------------------
-
-// const idDaTagClicada = document.getElementById("search-for");
-
-// function escolherOpcao() {
-// const pesquisar = document.getElementById("search-for");
-// const valorDaOpcao = pesquisar.options[pesquisar.selectedIndex].value;
-
-// return console.log(valorDaOpcao);
-// }
-
-// idDaTagClicada.addEventListener("click", escolherOpcao);
-
-// ------------------------
-
-//-------OH PALOMITA
-
-// console.log(data);
-// console.log(data.data.Aatrox);
-// const champName = Object.keys(data.data);
-// console.log(champName);
-// document.getElementById("champSection").innerHTML=champName.map(name=>`<img src="${data.data[name].img}">`)
-
-//------FIM DA OH PALOMITA
+//----------------------------------------------------------------------------------------[FIM] Monitorando select
