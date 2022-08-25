e61ee978ab77d44366db640e4fd7d458027ef5c1
lizandrafmiazaki <lizandramiazaki@gmail.com>
2022-08-25 15:55:45 -0300

    Tentativa de primeiro filter +ajuste imagem card CSS

--- src/data.js
+++ src/data.js
@@ -1,9 +1,35 @@
-// estas funciones son de ejemplo
+//exportando nossa constante com todas as funções de filtro
+export {dataLoL};
 
-export const example = () => {
-  return 'example';
-};
+//const com todos os filtros em forma de objeto.
+const dataLoL = {
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+  //Função de filtro por tipo de campeão
+  filtrarporTipo: function (todosOsDados, tags) {
+    const filtro = todosOsDados.filter(function(champ){
+      const filtroResultado = champ[tags].toLowerCase() === tags.toLowerCase();
+      return filtroResultado;
+    
+    });
+    return filtro
+
+  }
+
+
+}
+
+
+
+// const rickAndMorty = {
+
+//   // ===== Função de filtro por status ====
+//   filtrarPorStatus: function (list, status) {
+//       const filtro = list.filter(function(personagem) {        
+//           const statusFiltrado = personagem.status.toLowerCase() === status;         
+//           return statusFiltrado; 
+
+//       });             
+
+//       return filtro;  
+//   },
+// };
